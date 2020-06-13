c3806e1a3bafacd676a372140586213ded7ceb12
Kevin Bruton <kevin.jose.bs@gmail.com>
2020-06-13 09:41:43 +0200

    Separate front - back projects

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -5,21 +5,21 @@ const path = require('path');
 module.exports = ({ mode }) => {
   return {
     mode,
-    entry: './front/src/buffy-chart.js',
+    entry: './src/buffy-chart.js',
     output: {
-      path: path.resolve(__dirname, 'front', 'public'),
+      path: path.resolve(__dirname, 'dist'),
       filename: 'buffy-chart.js',
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './front/src/index.html',
+        template: './src/index.html',
       }),
       new CopyWebpackPlugin({
         patterns: [
           {
             context: 'node_modules/@webcomponents/webcomponentsjs',
             from: '**/*.js',
-            to: path.resolve(__dirname, 'front', 'public', 'webcomponents'),
+            to: path.resolve(__dirname, 'dist', 'webcomponents'),
           },
         ],
         options: {
