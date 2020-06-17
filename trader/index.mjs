00678dd4d691ede6f9b90485d47c06eac4532f8a
Kevin Bruton <kevin.jose.bs@gmail.com>
2020-06-17 11:26:30 +0200

    Add new structure for trader - saveTrade saveOpenPosition saveClosePosition

--- trader/index.mjs
+++ trader/index.mjs
@@ -1,50 +1,55 @@
 import path from 'path'
 import fs from 'fs'
 
-export {
-  openPosition,
-  closePosition
-}
-
-const openPositionsFile = path.join(path.resolve(), 'data', 'openPositions.json')
-
-/**
- * Open Position
- *
- * @param {'backtest'|'paper'|'live'} mode
- * @param {string} symbol
- * @param {string} provider
- * @param {number} quantity
- * @param {'buy'|'sell'} action
- * @param {'market'|'limit'} [type='market']
- * @param {number} [limit] - If type is 'limit' where to set the limit price
- * @returns {string} positionId
- */
-function openPosition(mode, symbol, provider, quantity, action, type = 'market', limit) {
-  const timestamp = (new Date()).valueOf()
-  const positionId = `${provider}_${symbol}_${action}_${timestamp}`
-  const newPos = {positionId, timestamp, mode, symbol, provider, quantity, action, type, limit}
-  addToOpenPositions(newPos)
-  return positionId
-}
-
-/**
- * Close a previously opened position by positionId
- *
- * @param {string} positionId - Eg. "bitfinex_BTCEUR_buy_1591657517651"
- * @returns
- */
-function closePosition(positionId) {
-  return removeFromOpenPositions(positionId)
-}
-
-function addToOpenPositions(newPos) {
-  const openPositions = JSON.parse(fs.readFileSync(openPositionsFile, {encoding: 'utf8'}))
-  fs.writeFileSync(openPositionsFile, JSON.stringify(openPositions.concat([newPos])))
-}
-
-function removeFromOpenPositions(positionId) {
-  const openPositions = JSON.parse(fs.readFileSync(openPositionsFile, {encoding: 'utf8'}))
-  const posIndex = openPositions.findIndex(pos => pos.positionId === positionId)
-  fs.writeFileSync(openPositionsFile, JSON.stringify(openPositions.slice(0, posIndex - 1).concat(openPositions.slice(posIndex + 1))))
-}
+export default (() => {
+  // At the moment there is only one mode:
+  // eslint-disable-next-line no-unused-vars
+  let mode
+  let symbol
+  let provider
+  let defaultQuantity
+  let tradesFile
+  let openPositionsFile
+  let initialBalance
+  let currentBalance
+  const saveTrade = ({action, quantity, price, type, limit, timestamp}) => {
+    fs.writeFileSync(tradesFile, `${action};${quantity};${price};${type};${limit};${timestamp};${currentBalance};
+`, {flag: 'a'})
+  }
+  const saveOpenPosition = newPos => {
+    const openPositions = JSON.parse(fs.readFileSync(openPositionsFile, {encoding: 'utf8'}))
+    saveTrade(newPos)
+    fs.writeFileSync(openPositionsFile, JSON.stringify(openPositions.concat([newPos]), null, 2))
+  }
+  return {
+    init: (initData) => {
+      mode = initData.mode || 'backtest'
+      symbol = initData.symbol
+      provider = initData.provider
+      defaultQuantity = initData.quantity
+      tradesFile = path.join(initData.tradeDir, 'trades.csv')
+      openPositionsFile = path.join(initData.tradeDir, 'openPositions.json')
+      fs.writeFileSync(tradesFile, 'action;quantity;price;type;limit;timestamp;balance;\n')
+      fs.writeFileSync(openPositionsFile, '[]')
+      initialBalance = Number(initData.initialBalance)
+      currentBalance = initialBalance
+    },
+    openPosition: ({action, price, type, limit, quantity}) => {
+      defaultQuantity = quantity || defaultQuantity
+      const timestamp = (new Date()).valueOf()
+      const positionId = `${provider}_${symbol}_${action}_${timestamp}`
+      const newPos = {positionId, timestamp, action, quantity: quantity || defaultQuantity, price, type: type || 'market', limit: limit || ''}
+      saveOpenPosition(newPos)
+      return positionId
+    },
+    closePosition: (positionId, closePrice) => {
+      const openPositions = JSON.parse(fs.readFileSync(openPositionsFile, {encoding: 'utf8'}))
+      const posIndex = openPositions.findIndex(pos => pos.positionId === positionId)
+      const position = openPositions[posIndex]
+      currentBalance += ((closePrice * position.quantity) - (position.price * position.quantity))
+      saveTrade({...position, ...{action: 'sell', price: closePrice}})
+      fs.writeFileSync(openPositionsFile, JSON.stringify(openPositions.slice(0, posIndex - 1).concat(openPositions.slice(posIndex + 1))))
+      return currentBalance
+    }
+  }
+})()
