09b46f695f23e366161794d3bc3159e0787984cb
Kevin Bruton <kevin.jose.bs@gmail.com>
2020-06-17 11:27:12 +0200

    Update dumb strategy to make trades

--- strategies/dumb.mjs
+++ strategies/dumb.mjs
@@ -1,22 +1,37 @@
-import util from 'util'
 import {sma} from '../indicators/tulind.mjs'
+import trader from '../trader/index.mjs'
 
 export default (() => {
-  let value = 0
-  let closeValues = []
+  const closeValues = []
+  let counter = 0
+  let trades = 0
+  let openPosId = 0
+  let balance = 0
   return {
-    init: () => {
+    init: async () => {
       console.log('run init')
-      value = 1
     },
     onTick: async candle => {
       closeValues.push(candle.close)
       // console.log('tick with candle', candle.timeDate)
       const smaResult = await sma(3, closeValues)
-      console.log(smaResult[smaResult.length - 1])
+      // console.log(smaResult[smaResult.length - 1])
+      if (counter % 10 === 0) {
+        if (!openPosId) {
+          openPosId = trader.openPosition({action: 'buy', price: candle.close})
+        } else {
+          balance = trader.closePosition(openPosId, candle.close)
+          openPosId = 0
+          trades += 1
+        }
+      }
+      counter += 1
     },
-    end: lastCandle => {
-      console.log('finish', value)
+    end: async lastCandle => {
+      if (openPosId) {
+        balance = trader.closePosition(openPosId, lastCandle.close)
+      }
+      console.log('Strategy finished.\nClosed', trades, 'positions\nFinal Balance:', Math.round(balance * 100) / 100)
     }
   }
 })()
